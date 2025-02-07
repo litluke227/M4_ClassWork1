50de974149eb899bcace3e6677588e5ef2df83cb
litluke227 <lucasm.lm95@gmail.com>
2025-02-07 10:56:56 -0500

    Update Commit

--- src/ClassWork.java
+++ src/ClassWork.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class ClassWork {
+    public static void main(String[] args)
+    {
+       int noPersons = 4;
+       double dishPrice = 18.99;
+       final double TAX_RATE = 0.2;
+       double subTotal = noPersons * dishPrice;
+       double tax = subTotal * TAX_RATE;
+       double tip = subTotal * 0.15;
+       double total = subTotal + tip + tax;
+        System.out.println("Total tax:"+ tax);
+        System.out.println("Total tip:"+ tip);
+        System.out.println("Total price:"+ total);
+        System.out.println("Each person owes:"+ total/noPersons);
     }
 }
